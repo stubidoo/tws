38d48b62f0771dcfcea13bee5f65c085e1d55064
stubidoo <stuart.glass2@gmail.com>
2020-04-07 16:17:22 +0100

    updated test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', async () => {
+test.skip('renders learn react link', async () => {
   const { getByText, getByTestId } = render(<App />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
   
   const roverA = getByTestId('1 3 N');
   console.log(roverA)
